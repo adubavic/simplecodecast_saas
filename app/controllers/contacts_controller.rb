1c818558c6f418ea44b0bbee9f7ffc8bf5d657a4
Victor Aduba <adubavic@yahoo.com>
2015-10-15 22:57:56 +0000

    created contact form view

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,21 @@
+class ContactsController < ApplicationController
+    
+    def new
+    @contact = Contact.new
+    end
+
+    def create
+     @contact = Contact.build(contact_params)
+    end
+def index
+end
+
+# define your contact params here
+
+private
+
+ def contact_params
+   params.require(:contact).permit(:name, :email, :comments)
+ end
+    
+end
